c8c03b371cb18d0fa19b9f83dbe790c8529ea401
Megan McKissack <meganmckissack@gmail.com>
2022-08-28 14:56:20 -0700

    add seeded data for api testing

--- CretaceousPark/Models/CretaceousParkContext.cs
+++ CretaceousPark/Models/CretaceousParkContext.cs
@@ -9,6 +9,18 @@ namespace CretaceousPark.Models
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+          builder.Entity<Animal>()
+          .HasData(
+          new Animal { AnimalId = 1, Name = "Matilda", Species = "Woolly Mammoth", Age = 7 },
+          new Animal { AnimalId = 2, Name = "Rexie", Species = "Dinosaur", Age = 10 },
+          new Animal { AnimalId = 3, Name = "Matilda", Species = "Dinosaur", Age = 2 },
+          new Animal { AnimalId = 4, Name = "Pip", Species = "Shark", Age = 4 },
+          new Animal { AnimalId = 5, Name = "Bartholomew", Species = "Dinosaur", Age = 22 }
+          );
+        }
+
         public DbSet<Animal> Animals { get; set; }
     }
 }
