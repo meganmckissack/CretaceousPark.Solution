58e469f330ca5221fbe6207b68fd64f0dcba6718
Megan McKissack <meganmckissack@gmail.com>
2022-08-29 18:57:54 -0700

    update animals controllers

--- CretaceousPark/Controllers/AnimalsController.cs
+++ CretaceousPark/Controllers/AnimalsController.cs
@@ -1,11 +1,136 @@
-using System;
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Threading.Tasks;
+// using Microsoft.AspNetCore.Http;
+// using Microsoft.AspNetCore.Mvc;
+// using Microsoft.EntityFrameworkCore;
+// using CretaceousPark.Models;
+
+// namespace CretaceousPark.Controllers
+// {
+//   [Route("api/[controller]")]
+//   [ApiController]
+//   public class AnimalsController : ControllerBase
+//   {
+//     private readonly CretaceousParkContext _db;
+
+//     public AnimalsController(CretaceousParkContext db)
+//     {
+//       _db = db;
+//     }
+
+//     // GET: api/Animals
+//     [HttpGet]
+//     public async Task<ActionResult<IEnumerable<Animal>>> Get(string species, string name)
+//     {
+//       var query = _db.Animals.AsQueryable();
+
+//       if (species != null)
+//       {
+//         query = query.Where(entry => entry.Species == species);
+//       }
+
+//       // if (gender != null)
+//       // {
+//       //   query = query.Where(entry => entry.Gender == gender);
+//       // }    
+
+//       if (name != null)
+//       {
+//         query = query.Where(entry => entry.Name == name);
+//       }      
+
+//       return await query.ToListAsync();
+//     }
+
+//     // GET: api/Animals/5
+//     [HttpGet("{id}")]
+//     public async Task<ActionResult<Animal>> GetAnimal(int id)
+//     {
+//         var animal = await _db.Animals.FindAsync(id);
+
+//         if (animal == null)
+//         {
+//             return NotFound();
+//         }
+
+//         return animal;
+//     }
+
+//     // PUT: api/Animals/5
+//     // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+//     [HttpPut("{id}")]
+//     public async Task<IActionResult> Put(int id, Animal animal)
+//     {
+//       if (id != animal.AnimalId)
+//       {
+//         return BadRequest();
+//       }
+
+//       _db.Entry(animal).State = EntityState.Modified;
+
+//       try
+//       {
+//         await _db.SaveChangesAsync();
+//       }
+//       catch (DbUpdateConcurrencyException)
+//       {
+//         if (!AnimalExists(id))
+//         {
+//           return NotFound();
+//         }
+//         else
+//         {
+//           throw;
+//         }
+//       }
+
+//       return NoContent();
+//     }
+
+//     // POST: api/Animals
+//     [HttpPost]
+//     public async Task<ActionResult<Animal>> Post(Animal animal)
+//     {
+//       _db.Animals.Add(animal);
+//       await _db.SaveChangesAsync();
+
+//       return CreatedAtAction(nameof(GetAnimal), new { id = animal.AnimalId }, animal);
+//     }
+
+//     // DELETE: api/Animals/5
+//     [HttpDelete("{id}")]
+//     public async Task<IActionResult> DeleteAnimal(int id)
+//     {
+//       var animal = await _db.Animals.FindAsync(id);
+//       if (animal == null)
+//       {
+//         return NotFound();
+//       }
+
+//       _db.Animals.Remove(animal);
+//       await _db.SaveChangesAsync();
+
+//       return NoContent();
+//     }
+
+//     private bool AnimalExists(int id)
+//     {
+//       return _db.Animals.Any(e => e.AnimalId == id);
+//     }
+//   }
+// }
+
+
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using CretaceousPark.Models;
+
+//note that our API app routes don't return views like in a web application
+
 
 namespace CretaceousPark.Controllers
 {
@@ -20,104 +145,21 @@ namespace CretaceousPark.Controllers
       _db = db;
     }
 
-    // GET: api/Animals
+    // GET api/animals -- Our GET route needs to return an ActionResult of type <IEnumerable<Animal>>. In our web applications, we didn't need to specify a type because we were always returning a view.
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Animal>>> Get(string species, string name)
+    public async Task<ActionResult<IEnumerable<Animal>>> Get()
     {
-      var query = _db.Animals.AsQueryable();
-
-      if (species != null)
-      {
-        query = query.Where(entry => entry.Species == species);
-      }
-
-      // if (gender != null)
-      // {
-      //   query = query.Where(entry => entry.Gender == gender);
-      // }    
-
-      if (name != null)
-      {
-        query = query.Where(entry => entry.Name == name);
-      }      
-
-      return await query.ToListAsync();
+      return await _db.Animals.ToListAsync();
     }
 
-    // GET: api/Animals/5
-    [HttpGet("{id}")]
-    public async Task<ActionResult<Animal>> GetAnimal(int id)
-    {
-        var animal = await _db.Animals.FindAsync(id);
-
-        if (animal == null)
-        {
-            return NotFound();
-        }
-
-        return animal;
-    }
-
-    // PUT: api/Animals/5
-    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-    [HttpPut("{id}")]
-    public async Task<IActionResult> Put(int id, Animal animal)
-    {
-      if (id != animal.AnimalId)
-      {
-        return BadRequest();
-      }
-
-      _db.Entry(animal).State = EntityState.Modified;
-
-      try
-      {
-        await _db.SaveChangesAsync();
-      }
-      catch (DbUpdateConcurrencyException)
-      {
-        if (!AnimalExists(id))
-        {
-          return NotFound();
-        }
-        else
-        {
-          throw;
-        }
-      }
-
-      return NoContent();
-    }
-
-    // POST: api/Animals
+    // POST api/animals -- Our POST route utilizes the function CreatedAtAction. This is so that it can end up returning the Animal object to the user, as well as update the status code to 201, for "Created", rather than the default 200 OK.
     [HttpPost]
     public async Task<ActionResult<Animal>> Post(Animal animal)
     {
       _db.Animals.Add(animal);
       await _db.SaveChangesAsync();
 
-      return CreatedAtAction(nameof(GetAnimal), new { id = animal.AnimalId }, animal);
-    }
-
-    // DELETE: api/Animals/5
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> DeleteAnimal(int id)
-    {
-      var animal = await _db.Animals.FindAsync(id);
-      if (animal == null)
-      {
-        return NotFound();
-      }
-
-      _db.Animals.Remove(animal);
-      await _db.SaveChangesAsync();
-
-      return NoContent();
-    }
-
-    private bool AnimalExists(int id)
-    {
-      return _db.Animals.Any(e => e.AnimalId == id);
+      return CreatedAtAction("Post", new { id = animal.AnimalId }, animal);
     }
   }
 }
